cfb9182a9f03943a805c1157e43ad45e7ff1617f
Phone Thiha Kyaw <mlsdphonethk@gmail.com>
2025-02-01 16:19:02 -0500

    build: add downward to dependencies

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,5 +1,7 @@
 include(cmake/CPM.cmake)
+
 CPMAddPackage("gh:TheLartians/PackageProject.cmake@1.13.0")
+
 CPMAddPackage(
   NAME ompl
   GITHUB_REPOSITORY ompl/ompl
@@ -10,6 +12,7 @@ CPMAddPackage(
       "OMPL_BUILD_PYBINDINGS OFF"
       "OMPL_BUILD_PYTESTS OFF"
 )
+
 CPMAddPackage(
   NAME behaviortree_cpp
   GITHUB_REPOSITORY BehaviorTree/BehaviorTree.CPP
@@ -19,6 +22,7 @@ CPMAddPackage(
       "BTCPP_UNIT_TESTS OFF"
       "BTCPP_EXAMPLES OFF"
 )
+
 CPMAddPackage(
   NAME yaml-cpp
   GITHUB_REPOSITORY jbeder/yaml-cpp
@@ -28,4 +32,40 @@ CPMAddPackage(
     "YAML_CPP_BUILD_TESTS OFF"
     "YAML_CPP_BUILD_CONTRIB OFF"
     "YAML_CPP_BUILD_TOOLS OFF"
+    "YAML_CPP_INSTALL ON"
 )
+
+CPMAddPackage(
+  NAME downward
+  GITHUB_REPOSITORY aibasel/downward
+  GIT_TAG release-24.06.0
+  DOWNLOAD_ONLY YES
+)
+if (downward_ADDED)
+  # Copy the translator into the output directory
+  add_custom_target(translate ALL)
+  add_custom_command(TARGET translate POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy_directory
+          ${downward_SOURCE_DIR}/src/translate
+          ${downward_BINARY_DIR}/translate
+      COMMENT "Copying translator module into output directory")
+
+  # Copy fast-downward.py script
+  add_custom_target(copy_fast_downward ALL)
+  add_custom_command(TARGET copy_fast_downward POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy
+          ${downward_SOURCE_DIR}/fast-downward.py
+          ${downward_BINARY_DIR}/fast-downward.py
+      COMMENT "Copying fast-downward.py into output directory")
+
+  # Copy the driver into the output directory
+  add_custom_target(copy_driver ALL)
+  add_custom_command(TARGET copy_driver POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy_directory
+          ${downward_SOURCE_DIR}/driver
+          ${downward_BINARY_DIR}/driver
+      COMMENT "Copying driver module into output directory")
+
+  # Add search component as a subproject.
+  add_subdirectory(${downward_SOURCE_DIR}/src/search ${downward_BINARY_DIR}) 
+endif()
